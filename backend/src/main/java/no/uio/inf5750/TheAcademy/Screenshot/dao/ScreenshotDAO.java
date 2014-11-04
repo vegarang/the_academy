05c76930164a117ddf818fa06c7cebb4de91beb6
Mats Schjølberg <mats@schjolberg.net>
2014-11-04 20:54:34 +0100

    Added some javadoc and functions in the interfaces

--- backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/dao/ScreenshotDAO.java
+++ backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/dao/ScreenshotDAO.java
@@ -1,5 +1,6 @@
 package no.uio.inf5750.TheAcademy.Screenshot.dao;
 
+import java.util.Collection;
 import java.util.List;
 
 import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
@@ -10,8 +11,28 @@ import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
  *
  */
 public interface ScreenshotDAO {
-	void addScreenshot(String userId, Screenshot screen);
+	/**
+	 * Function for saving screenshot
+	 * @param userId
+	 * @param screen
+	 * @return Id of the screenshot
+	 */
+	String addScreenshot(String userId, Screenshot screen);
+	/**
+	 * Get the screenshot from database
+	 * @param screenshotId
+	 * @return Screenshot
+	 */
 	Screenshot getScreenshot(String screenshotId);
-	List<Screenshot> getScreenshots(String userId);
-	
+	/**
+	 * Get all the screenshot from user
+	 * @param userId
+	 * @return Collection of screenshots
+	 */
+	Collection<Screenshot> getScreenshots(String userId);
+	/**
+	 * Delete the screenshot
+	 * @param screenshotId
+	 */
+	void deleteScreenshot(String screenshotId);
 }
