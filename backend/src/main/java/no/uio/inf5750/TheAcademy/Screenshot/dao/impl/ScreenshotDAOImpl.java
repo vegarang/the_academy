28e03a8a65f2aba9038235b4e518a0f3e2016cd3
Mats Schjølberg <mats@schjolberg.net>
2014-11-05 17:07:14 +0100

    Forgot to add the classes before the commit

---
+++ backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/dao/impl/ScreenshotDAOImpl.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package no.uio.inf5750.TheAcademy.Screenshot.dao.impl;
+
+import java.util.Collection;
+
+import no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO;
+import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
+
+/**
+ * @author mats
+ *
+ */
+public class ScreenshotDAOImpl implements ScreenshotDAO {
+
+	/* (non-Javadoc)
+	 * @see no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO#addScreenshot(no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot)
+	 */
+	public String addScreenshot(Screenshot screen) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO#getScreenshot(java.lang.String)
+	 */
+	public Screenshot getScreenshot(String screenshotId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO#getScreenshots(java.lang.String)
+	 */
+	public Collection<Screenshot> getScreenshots(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO#deleteScreenshot(java.lang.String)
+	 */
+	public void deleteScreenshot(String screenshotId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/models/impl/ScreenshotImpl.java
@@ -0,0 +1,42 @@
+package no.uio.inf5750.TheAcademy.Screenshot.models.impl;
+
+import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
+
+public class ScreenshotImpl implements Screenshot {
+
+	public String getUserId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getComment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void editComment(String newComment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void appendComment(String comment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getUrl() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getCanvas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
