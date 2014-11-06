2a425f49265eebbac56701743ee0a2cb23bd5ff8
Mats Schjølberg <mats@schjolberg.net>
2014-11-06 21:16:34 +0100

    wrote some code

--- backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/models/impl/ScreenshotImpl.java
+++ backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/models/impl/ScreenshotImpl.java
@@ -3,40 +3,38 @@ package no.uio.inf5750.TheAcademy.Screenshot.models.impl;
 import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
 
 public class ScreenshotImpl implements Screenshot {
+	private String userId;
+	private String comment;
+	private String url;
+	private String canvas;
+	private String title;
 
 	public String getUserId() {
-		// TODO Auto-generated method stub
-		return null;
+		return userId;
 	}
 
 	public String getComment() {
-		// TODO Auto-generated method stub
-		return null;
+		return comment;
 	}
 
 	public void editComment(String newComment) {
-		// TODO Auto-generated method stub
-
+		comment = newComment;
 	}
 
 	public void appendComment(String comment) {
-		// TODO Auto-generated method stub
-
+		this.comment += "\n" + comment;
 	}
 
 	public String getUrl() {
-		// TODO Auto-generated method stub
-		return null;
+		return url;
 	}
 
 	public String getCanvas() {
-		// TODO Auto-generated method stub
-		return null;
+		return canvas;
 	}
 
 	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		return title;
 	}
 
 }
