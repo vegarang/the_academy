a103e1a864516cd9caecefb96876c0c81e85393b
Mats Schjølberg <mats@schjolberg.net>
2014-11-13 20:03:19 +0100

    added spring dependencies, added properties for version handling spring and hibernate, startet hibernate implementation

--- backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/dao/impl/ScreenshotDAOImpl.java
+++ backend/src/main/java/no/uio/inf5750/TheAcademy/Screenshot/dao/impl/ScreenshotDAOImpl.java
@@ -5,6 +5,11 @@ package no.uio.inf5750.TheAcademy.Screenshot.dao.impl;
 
 import java.util.Collection;
 
+
+import org.hibernate.SessionFactory;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO;
 import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
 
@@ -12,7 +17,13 @@ import no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot;
  * @author matsasc 	
  *
  */
+@Transactional
 public class ScreenshotDAOImpl implements ScreenshotDAO {
+	SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
 
 	/* (non-Javadoc)
 	 * @see no.uio.inf5750.TheAcademy.Screenshot.dao.ScreenshotDAO#addScreenshot(no.uio.inf5750.TheAcademy.Screenshot.models.Screenshot)
