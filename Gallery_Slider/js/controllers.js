018a88d23abf3e71dc09336714697b93c93a7e02
Hodoea <hodoea@student.matnat.uio.no>
2014-11-19 12:56:04 +0100

    updated gallery

---
+++ Gallery_Slider/js/controllers.js
@@ -0,0 +1,12 @@
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope,$http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+    //$scope.phones = data.splice(0, 5);
+  });
+
+  $scope.name = "World";
+  $scope.orderProp = 'age';
+});
