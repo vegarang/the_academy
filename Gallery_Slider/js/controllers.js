0d4ba8299b2bd3cdf2c14737ee47247b857e54bf
Vegard Angell <vegard.angell@mn.uio.no>
2014-12-07 18:57:32 +0100

    Added next and previous buttons, and some minor formatting tweaks to adapt the test-data to what we will actually receive from server

--- Gallery_Slider/js/controllers.js
+++ Gallery_Slider/js/controllers.js
@@ -1,111 +1,73 @@
-angular.module('sliderApp.controllers',[])
-.controller('SliderController', function($scope,$modal) {
-    
-    $scope.open = function(image,index) {
+angular.module('sliderApp.controllers', [])
+    .controller('SliderController', function ($scope, $modal) {
 
-        $modal.open({
-            templateUrl:'imageslide.html',
-            controller: 'ImgCtrl',
-            resolve: {
-                image: function () {
-                    return $scope.images[index];
+        $scope.open = function (image, index) {
+
+            $modal.open({
+                templateUrl: 'imageslide.html',
+                controller: 'ImgCtrl',
+                size: 'lg',
+                resolve: {
+                    data: function () {
+                        return {images: $scope.images, index: index};
+                    }
                 }
+            }).result.then(function () {
+                    console.log("modal closed..");
+                }, function () {
+                    console.log("modal dismissed");
+                });
+        };
+
+        $scope.images = [
+            {
+                src: 'img/dv3.png',
+                title: 'Pic 1',
+                description: "Some description here",
+                url: 'www.example.com/north/pole',
+                date: '2014-12-24',
+                username: 'julenissen'
+            }, {
+                src: 'img/gis4.png',
+                title: 'Pic 2',
+                description: "description here",
+                url: 'www.example.com/is/it/a/bird?or&a&plane',
+                date: '1950-01-01',
+                username: 'superman'
+            }, {
+                src: 'img/gis5.png',
+                title: 'Pic 3',
+                description: "Other description here",
+                url: 'www.example.com/na/na/na/',
+                date: '2014-01-01',
+                username: 'batman'
             }
-        }).result.then(function(){
-            console.log("modal closed..");
-        }, function() {
-            console.log("modal dismissed");
-        });
-    }
-    
-    $scope.images=[
-        {
-            src:'img/dv3.png',
-            title:'Pic 1',
-            active: false
-        },{
-            src:'img/gis4.png',
-            title:'Pic 2',
-            active: false
-        },{
-            src:'img/gis5.png',
-            title: 'Pic 3',
-            active: false
-        },{
-            src:'img/dv3.png',
-            title:'Pic 1',
-            active: false
-        },{
-            src:'img/gis4.png',
-            title:'Pic 2',
-            active: false
-        },{
-            src:'img/gis5.png',
-            title: 'Pic 3',
-            active: false
-        },{
-            src:'img/dv3.png',
-            title:'Pic 1',
-            active: false
-        },{
-            src:'img/gis4.png',
-            title:'Pic 2',
-            active: false
-        },{
-            src:'img/gis5.png',
-            title: 'Pic 3',
-            active: false
-        },{
-            src:'img/dv3.png',
-            title:'Pic 1',
-            active: false
-        },{
-            src:'img/gis4.png',
-            title:'Pic 2',
-            active: false
-        },{
-            src:'img/gis5.png',
-            title: 'Pic 3',
-            active: false
-        },{
-            src:'img/dv3.png',
-            title:'Pic 1',
-            active: false
-        },{
-            src:'img/gis4.png',
-            title:'Pic 2',
-            active: false
-        },{
-            src:'img/gis5.png',
-            title: 'Pic 3',
-            active: false
-        },{
-            src:'img/dv3.png',
-            title:'Pic 1',
-            active: false
-        },{
-            src:'img/gis4.png',
-            title:'Pic 2',
-            active: false
-        },{
-            src:'img/gis5.png',
-            title: 'Pic 3',
-            active: false
-        }
-    ];
-})
+        ];
+    })
 
-.controller('ImgCtrl',function($scope,$modalInstance,image) {
+    .controller('ImgCtrl', function ($scope, $modalInstance, data) {
 
+        $scope.images = data.images;
+        $scope.cur_index = data.index;
 
-    $scope.image = image;
-    
-    $scope.cancel = function() {
-        $modalInstance.close()
+        $scope.cancel = function () {
+            $modalInstance.close()
+        };
 
-    }
+        $scope.next = function () {
+            $scope.cur_index++;
+            if ($scope.cur_index >= $scope.images.length) {
+                $scope.cur_index = 0;
+            }
+        };
 
-});
+        $scope.previous = function () {
+            $scope.cur_index--;
+            if ($scope.cur_index < 0) {
+                $scope.cur_index = $scope.images.length - 1;
+            }
+        };
+    });
 
 
 // angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
@@ -115,7 +77,7 @@ angular.module('sliderApp.controllers',[])
 //     var newWidth = 600 + slides.length + 1;
 //     slides.push({
 //       image: 'http://placekitten.com/' + newWidth + '/300',
-   
+
 //     });
 //   };
 //   for (var i=0; i<4; i++) {
